441eeac6130cf74ab12eb1d1bb12b0306fc15d81
Gregg <greggubben@gmail.com>
2014-12-31 15:44:55 -0500

    initial Face Tuning code

---
+++ FaceTuning.ino
@@ -0,0 +1,328 @@
+/*
+ * Tuning operations for the Face
+ *
+ * The servo under test must be attached to PIN 11
+ *
+ * List of tests:
+ *     Set the Servo to 90 degrees dddd
+ */
+#include <Servo.h>
+
+// Test selection pins
+boolean testRange = false;
+
+// Pot input for the Range Test
+const int POT_PIN = 1;  // Use a pot to see the range of the servo
+
+// Range of Servos available for testing
+const int MIN_SERVO_PIN = 2;
+const int MAX_SERVO_PIN = 10;
+
+// Last position of the servo.  Used to minimize communication
+int last_servoVal;
+int min_servoVal[MAX_SERVO_PIN+1];
+int max_servoVal[MAX_SERVO_PIN+1];
+
+// Pin of the servo being tested
+Servo testServo[14];
+int servoPin = 0;
+
+// LED to show we are running a test
+const int RUNNING_TEST_PIN = 13;  // Position the Servo to 90 degrees
+
+void setup() {
+  // For writing the results
+  Serial.begin(9600);
+  
+  // Pin used to show Test is being performed
+  pinMode(RUNNING_TEST_PIN, OUTPUT);
+
+  quitTesting();
+  detachAllServos();
+  clearAllValues();
+  servoPin = 0;
+  printHelp();
+}
+
+void loop() {
+  // Get what to do
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    int pin = 0;
+    
+    switch (inByte) {
+      case 'h':
+      case '?':                // Print the help
+        printHelp();
+        break;
+
+      case 'R':                // Reset everything
+        Serial.println("Resetting... (calling setup)");
+        setup();
+        break;
+        
+    case 's':                  // Select the servo pin to test
+      pin = Serial.parseInt();
+      if (pin < MIN_SERVO_PIN || pin > MAX_SERVO_PIN) {
+        Serial.println("Invalid Pin");
+        Serial.println();
+      }
+      else {
+        quitTesting();
+        printValues();
+        Serial.println();
+        setPin(pin);
+      }
+        break;
+
+    case 't':                  // Test Range
+      if (servoPin < MIN_SERVO_PIN || servoPin > MAX_SERVO_PIN) {
+        Serial.println("Invalid Pin");
+      }
+      else {
+        startRangeTest();
+      }
+      break;
+
+    case 'n':                  // Next Servo to Test Range
+      if (servoPin < MIN_SERVO_PIN) {
+        // Have not started yet - init
+        servoPin = MIN_SERVO_PIN;
+      }
+      else {
+        // proceed to next servo
+        quitTesting();
+        servoPin++;
+        Serial.println();
+      }
+
+      if (servoPin > MAX_SERVO_PIN) {
+        // Reached the end - done
+        printAllValues();
+      }
+      else {
+        // start testing next servo
+        setPin(servoPin);
+        startRangeTest();
+      }
+      break;
+
+    case 'q':                  // Quit testing
+      quitTesting();
+      printValues();
+      break;
+
+    case '9':                  // Set the servo back to 90
+      quitTesting();
+      setServoTo90();
+      break;
+
+    case 'I':                  // Set All servos back to 90
+      quitTesting();
+      setAllServosTo90();
+      servoPin = 0;
+      break;
+
+    case 'd':                  // Detach servo
+      quitTesting();
+      detachServo();
+      servoPin = 0;
+      Serial.println();
+      break;
+
+    case 'D':                  // Detach All servos
+      quitTesting();
+      detachAllServos();
+      servoPin = 0;
+      Serial.println();
+      break;
+      
+    case 'c':                  // Clear the key servo values
+      clearValues();
+      break;
+      
+    case 'C':                  // Clear All servos key values
+      clearAllValues();
+      break;
+      
+    case 'p':                  // Print the key servo values
+      printValues();
+      break;
+      
+    case 'P':                  // Print the key servo values
+      quitTesting();         // Do it for all servos
+      Serial.println();
+      printAllValues();
+      Serial.println();
+      break;
+      
+    default:
+      break;
+    }
+  }
+
+  // Perform the tests
+  if (testRange) {
+    int potVal = analogRead(POT_PIN);   // reads the value of the potentiometer (value between 0 and 1023) 
+    int servoVal = map(potVal, 0, 1023, 0, 179);    // scale it to use it with the servo (value between 0 and 180) 
+    if (servoVal != last_servoVal) {
+      Serial.print("Servo Pos: ");
+      Serial.print(servoVal);
+      Serial.println();
+      last_servoVal = servoVal;
+      testServo[servoPin].write(servoVal);
+      if (min_servoVal[servoPin] > servoVal) {
+        min_servoVal[servoPin] = servoVal;
+      }
+      if (max_servoVal[servoPin] < servoVal) {
+        max_servoVal[servoPin] = servoVal;
+      }
+      delay(15);                             // waits for the servo to get there
+    }
+  }
+}
+
+///////////////////////
+//
+// Supporting Functions
+//
+///////////////////////
+
+
+// Print the list of commands available
+void printHelp() {
+  Serial.println();
+  Serial.println("Commands:");
+  Serial.println("  h  = Help (this menu)");
+  Serial.println("  ?  = Help (this menu)");
+  Serial.println();
+  Serial.println("  R  = Reset everything");
+  Serial.println();
+  Serial.println("  s# = Servo where # is the number of the pin (2-13) the servo is attached");
+  Serial.println("  t  = Test range");
+  Serial.println("  n  = Next servo to test range");
+  Serial.println("  q  = Quit testing");
+  Serial.println();
+  Serial.println("  9  = set the servo to 90 degrees");
+  Serial.println("  I  = initialize All servos to 90 degrees");
+  Serial.println();
+  Serial.println("  d  = Detach servo");
+  Serial.println("  D  = Detach All servos to detached state");
+  Serial.println();
+  Serial.println("  c  = Clear the key servo testing values");
+  Serial.println("  C  = Clear All the key servo testing values");
+  Serial.println("  p  = Print the key servo testing values");
+  Serial.println("  P  = print All the key servo testing values");
+  Serial.println();
+}
+
+// Set a new Servo Pin to test with
+void setPin(int pin) {
+  servoPin = pin;
+  if (!testServo[servoPin].attached()) {
+    testServo[servoPin].attach(servoPin);
+  }
+  
+  Serial.print("Servo Pin = ");
+  Serial.print(servoPin);
+  Serial.println();
+  setServoTo90();
+}
+
+// Start the range testing
+void startRangeTest() {
+  if (testRange) {
+    Serial.println("Already Testing");
+  }
+  else {
+    testRange = true;
+    digitalWrite(RUNNING_TEST_PIN,HIGH);
+    Serial.println("Testing Started");
+  }
+}
+
+// Quit running the Range testing
+void quitTesting() {
+  if (testRange) {
+    testRange = false;
+    digitalWrite(RUNNING_TEST_PIN,LOW);
+    Serial.println("Testing Stopped");
+  }
+}
+
+// Initialize all Servos to 90 degrees
+void setAllServosTo90 () {
+  Serial.println("Initializing All Servos to 90 degrees");
+  for (servoPin = MIN_SERVO_PIN; servoPin<=MAX_SERVO_PIN; servoPin++) {
+    setPin(servoPin);
+  }
+  Serial.println();
+}
+
+// Set the Servo to 90 degrees
+void setServoTo90 () {
+  testServo[servoPin].write(90);
+  Serial.print("Servo on Pin = ");
+  Serial.print(servoPin);
+  Serial.print(" set to 90");
+  Serial.println();
+  delay(15);
+}
+
+// Detach Servos
+void detachServo () {
+  if (testServo[servoPin].attached()) {
+    testServo[servoPin].detach();
+    Serial.print("Detached Servo on Pin = ");
+    Serial.print(servoPin);
+    Serial.println();
+  }
+}
+
+// Reset all Servos to detached
+void detachAllServos () {
+  Serial.println("Detaching All Servos");
+  for (servoPin = MIN_SERVO_PIN; servoPin<=MAX_SERVO_PIN; servoPin++) {
+    detachServo();
+  }
+}
+
+// Clear the servo key values
+void clearValues() {
+  min_servoVal[servoPin] = 255;
+  max_servoVal[servoPin] = 0;
+  Serial.print("Values Cleared for Servo on Pin = ");
+  Serial.print(servoPin);
+  Serial.println();
+}
+
+// Clear all Servo key values
+void clearAllValues () {
+  Serial.println("Clearing All Servos");
+  for (servoPin = MIN_SERVO_PIN; servoPin<=MAX_SERVO_PIN; servoPin++) {
+    clearValues();
+  }
+}
+
+// Print the servo key values
+void printValues() {
+  Serial.print("Servo Pin = ");
+  Serial.print(servoPin);
+//  Serial.println();
+  Serial.print("; Min Pos = ");
+  Serial.print(min_servoVal[servoPin]);
+//  Serial.println();
+  Serial.print("; Max Pos = ");
+  Serial.print(max_servoVal[servoPin]);
+  Serial.println();
+}
+
+// Print the all the servo key values
+void printAllValues() {
+  int remember_servoPin = servoPin;
+  Serial.println("Printing All Servo Values");
+  for (servoPin = MIN_SERVO_PIN; servoPin<=MAX_SERVO_PIN; servoPin++) {
+    printValues();
+  }
+  servoPin = remember_servoPin;
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+Robot Face tuning code for the Arduino.
+
+Inspired by the DIY example by Chris and published on http://www.pyroelectro.com/animatronics-how-to/
+
+Created by: Gregg Ubben
+
+The following are the connections:
+
+| Face Component          | Pin        |
+| ----------------------- | ---------- |
+| Left Eye Brow           | Digital 2  |
+| Right Eye Brow          | Digital 3  |
+| Left Eye Side to Side   | Digital 4  |
+| Right Eye Side to Side  | Digital 5  |
+| Left Eye Up/Down        | Digital 6  |
+| Right Eye Up/Down       | Digital 7  |
+| Mouth                   | Digital 8  |
+| Neck Up/Down            | Digital 9  |
+| Neck Side to Side       | Digital 10 |
+| Tuning Pot Side to Side | Analog 0   |
+| Tuning Pot Up/Down      | Analog 1   |
+| LCD SDA                 | Analog 4   |
+| LCD SCL                 | Analog 5   |
+
